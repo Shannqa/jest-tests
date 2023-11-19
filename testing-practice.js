90dabc29fbab34b7e5ae65bff53a6fd8c05371b4
Shannqa <shannqa@gmail.com>
2023-11-19 22:46:54 +0100

    Finish up exercises

--- testing-practice.js
+++ testing-practice.js
@@ -11,7 +11,86 @@ function reverseString(string) {
   return newString;
 }
 
-reverseString("kitty goes home");
+const calculator = {
+  sum: function (num1, num2) {
+    return num1 + num2;
+  },
+  substract: function (num1, num2) {
+    return num1 - num2;
+  },
+  divide: function (num1, num2) {
+    return num1 / num2;
+  },
+  multiply: function (num1, num2) {
+    return num1 * num2;
+  },
+};
+
+function caesarCipher(shift, string) {
+  let encodedString = "";
+  let shiftedLetter;
+  for (let i = 0; i < string.length; i++) {
+    // A-Z codes 65-90
+    // a-z codes 97-122
+
+    let plain = string.charCodeAt(i);
+    if (plain >= 65 && plain <= 90) {
+      shiftedLetter = ((plain - 65 + shift) % 26) + 65;
+      let encodedLetter = String.fromCharCode(shiftedLetter);
+      encodedString += encodedLetter;
+    } else if (plain >= 97 && plain <= 122) {
+      shiftedLetter = ((plain - 97 + shift) % 26) + 97;
+      let encodedLetter = String.fromCharCode(shiftedLetter);
+      encodedString += encodedLetter;
+    } else {
+      encodedString += string[i];
+    }
+  }
+  return encodedString;
+}
+
+function analyzeArray(array) {
+  function minimum(array) {
+    let min = array[0];
+    array.forEach((number) => {
+      if (number < min) {
+        min = number;
+      }
+    });
+    return min;
+  }
+
+  function maximum(array) {
+    let max = array[0];
+    array.forEach((number) => {
+      if (number > max) {
+        max = number;
+      }
+    });
+    return max;
+  }
+
+  function average(array) {
+    let sum = array.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    return sum / array.length;
+  }
+
+  const object = {
+    average: average(array),
+    min: minimum(array),
+    max: maximum(array),
+    length: array.length,
+  };
+
+  return object;
+}
+
+console.log(analyzeArray([77, 24, 14, 66, 53, 12]));
 
 module.exports.capitalize = capitalize;
 module.exports.reverseString = reverseString;
+module.exports.calculator = calculator;
+module.exports.caesarCipher = caesarCipher;
+module.exports.analyzeArray = analyzeArray;
--- testing-practice.test.js
+++ testing-practice.test.js
@@ -1,5 +1,8 @@
 const { capitalize } = require("./testing-practice.js");
 const { reverseString } = require("./testing-practice.js");
+const { calculator } = require("./testing-practice.js");
+const { caesarCipher } = require("./testing-practice.js");
+const { analyzeArray } = require("./testing-practice.js");
 
 test("capitalize the first letter of a string", () => {
   expect(capitalize("go home")).toBe("Go home");
@@ -7,4 +10,35 @@ test("capitalize the first letter of a string", () => {
 
 test("reverse letters in a string", () => {
   expect(reverseString("kitty")).toBe("yttik");
+});
+
+test("calculate the sum of two numbers", () => {
+  expect(calculator.sum(1, 4)).toBe(5);
+});
+
+test("calculate the substraction of two numbers", () => {
+  expect(calculator.substract(5, 2)).toBe(3);
+});
+
+test("calculate the division of two numbers", () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test("calculate the multiplication of two numbers", () => {
+  expect(calculator.multiply(8, 3)).toBe(24);
+});
+
+test("encode the string using caesar code and the specified shift", () => {
+  expect(
+    caesarCipher(5, "happiness is a choice that requires effort at times.")
+  ).toBe("mfuunsjxx nx f hmtnhj ymfy wjvznwjx jkktwy fy ynrjx.");
+});
+
+test("analyze an array of numbers, return an object with properties average, min, max, length", () => {
+  expect(analyzeArray([77, 24, 14, 66, 53, 12])).toStrictEqual({
+    average: 41,
+    min: 12,
+    max: 77,
+    length: 6,
+  });
 });
